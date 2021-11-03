d9f5609bb3a4b99002197c0205799775cd2aa7b1
unknown <rodrigo.toblli@gmail.com>
2021-11-03 09:26:16 -0300

    service controller tests

--- tests/unit/controllers/cart-test.js
+++ tests/unit/controllers/cart-test.js
@@ -4,9 +4,18 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Controller | cart', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
     let controller = this.owner.lookup('controller:cart');
     assert.ok(controller);
+  });
+  test('bought amount items', function (assert) {
+    let controller = this.owner.lookup('controller:cart');
+    let service = this.owner.lookup('service:cart');
+    let greenTea = { id: 'GR1'}
+    let strawberry = { id: 'SR1'}
+    service.addProductToCart(greenTea)
+    service.addProductToCart(strawberry)
+    assert.equal(controller.boughtItems, 2, 'two bought items');
+    assert.equal(controller.itemsAmount, 3, 'two bought items, one free product'); 
   });
 });
--- tests/unit/services/cart-test.js
+++ tests/unit/services/cart-test.js
@@ -4,9 +4,39 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Service | cart', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
     let service = this.owner.lookup('service:cart');
     assert.ok(service);
   });
+
+  test('list initial as empty', function (assert) {
+    let service = this.owner.lookup('service:cart');
+    assert.equal(service.productsList.length, 0, 'empty products list');
+  });
+
+  test('add green Tea offer', function (assert) {
+    let service = this.owner.lookup('service:cart');
+    let greenTea = { id: 'GR1'}
+    service.addProductToCart(greenTea)
+    assert.equal(service.productsList[0].freeProduct, 1, 'products list has freeTea');
+  });
+
+  test('add the same product two times', function (assert) {
+    let service = this.owner.lookup('service:cart');
+    let greenTea = { id: 'GR1'}
+    service.addProductToCart(greenTea)
+    assert.equal(service.productsList[0].count, 1, 'count 1');
+    service.addProductToCart(greenTea)
+    assert.equal(service.productsList[0].count, 2, 'count 2');
+  });
+
+  test('add and remove item', function (assert) {
+    let service = this.owner.lookup('service:cart');
+    let greenTea = { id: 'GR1'}
+    service.addProductToCart(greenTea)
+    assert.equal(service.productsList[0].count, 1, 'count 1');
+    service.removeProductFromCart(greenTea)
+    assert.equal(service.productsList.length, 0, 'empty products list');
+  });
+
 });
